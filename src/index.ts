520114d6660856bd50de0de1c42678b30a62cff5
MyNameAmir <60043971+MyNameAmir@users.noreply.github.com>
2021-07-23 14:53:32 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ import app from './app';
 const port = process.env.port;
 const server = app.listen(process.env.port);
 
+app.use('/favicon.ico', (req, res) => {
+    res.status(503).end();
+});
+
+
+app.use('/', (req, res) => {
+    res.status(503).end();
+});
+
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
